242a374fc30dd6102027790a245b023352b781a1
Moeed Rajpoot <moeedrajpoot1@gmail.com>
2026-02-22 02:57:37 +0500

    Remove nested repo and add arrays as normal folder

---
+++ arrays/arrays.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+	// numberd sequence of specific lenght
+
+	// var nums [4]int
+
+	//fmt.Println("this is length", len(nums))
+
+	// nums[0] = 1
+
+	// fmt.Println("numss>>>>", nums[0])
+	// fmt.Println("this is array", nums)
+
+	// // bolean always false if not assigned
+	// var boolean [3]bool
+	// boolean[2] = true
+	// fmt.Println("helloooo>>>>>>", boolean)
+	// fmt.Println(">>>>>>", boolean[2])
+
+	// to declare in single line
+	// num := [3]int{1, 2, 3}
+
+	// fmt.Println("this is num>>>>", num)
+
+	// 2s array
+
+	nums := [2][2]int{{3, 4}, {5, 6}}
+
+	fmt.Println("this is 2D array", nums)
+
+}
